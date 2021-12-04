be325ea6395b31a8f2cc00036e6c56bc2afe94f9
msm <msm@tailcall.net>
2021-12-04 20:19:48 +0100

    Day 04: Racket

---
+++ 04/1.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(define (transpose xss) (apply map list xss))
+(define (score xss) (apply + (flatten xss)))
+(define (zeroes? xss) (andmap zero? xss))
+(define (wins? xss) (or (ormap zeroes? xss) (ormap zeroes? (transpose xss))))
+(define (replace xss x with) (map (lambda (e) (if (= x e) with e)) xss)) 
+(define (replace2d xss x with) (map (lambda (e) (replace e x with)) xss))
+
+(define (tobingo list) (map (lambda (l) (map string->number (string-split l))) list))
+(define (bingos list) (if (empty? list)
+    '()
+    (cons (tobingo (take (cdr list) 5)) (bingos (drop list 6)))))
+(define (play bingo nums round last) (if (wins? bingo)
+    (cons round (* last (score bingo)))
+    (play (replace2d bingo (car nums) 0) (cdr nums) (+ 1 round) (car nums))))
+
+(let* (
+    [test (file->lines "input")]
+    [numbers (map string->number (string-split (car test) ","))]
+    [lines (cdr test)]
+    [play1 (lambda (b) (play b numbers 0 0))])
+    (println (cdar (sort (map play1 (bingos lines)) < #:key car))))
---
+++ 04/README.md
@@ -0,0 +1,16 @@
+# Day 04: Racket
+
+Time to dust off my old SICP book, scheme is back on the menu.
+
+This time I've decided to do something less cursed than Jsonnet, but harder
+than just throwing C# at it. I've always wanted to try racket, so it felt like
+a good opportunity. On the other hand, I feel like I didn't really use the
+full power of the language - I mean, there's not a single macro in my source.
+And racket calls it self "language oriented programming language".
+
+Well, maybe another time.
+
+PS. Oh, BTW my solutions aren't correct - instead of keeping track of crossed
+out elements, i replace them to zero. This is almost correct, except zero is a
+valid input number - so it can make board win easier than it should. Works for
+the provided input though, so good enough.
