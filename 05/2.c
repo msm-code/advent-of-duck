fb0ef0ceab0e3ab860e5244879f3f5f43e153398
msm <msm@tailcall.net>
2021-12-05 20:50:46 +0100

    Day 05: C

---
+++ 05/2.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#define SWAP(X, Y) (X ^= Y ^= X ^= Y)
+
+uint8_t ocean[1024][1024];
+
+int main() {
+    while (true) {
+        int x1, y1, x2, y2;
+        if (scanf("%d,%d -> %d,%d", &x1, &y1, &x2, &y2) < 0) break;
+
+        if (x1 == x2) {
+            if (y1 > y2) { SWAP(y1, y2); }
+            while(y1 <= y2) ocean[x1][y1++]++;
+        } else if (y1 == y2) {
+            if (x1 > x2) { SWAP(x1, x2); }
+            while(x1 <= x2) ocean[x1++][y1]++;
+        } else if ((x2 > x1) == (y2 > y1)) {
+            if (x1 > x2) { SWAP(x1, x2); SWAP(y1, y2); }
+            while(x1 <= x2) ocean[x1++][y1++]++;
+        } else if ((x2 > x1) != (y2 > y1)) {
+            if (x1 > x2) { SWAP(x1, x2); SWAP(y1, y2); }
+            while(x1 <= x2) ocean[x1++][y1--]++;
+        }
+    }
+    int total = 0;
+    for (int x = 0; x < 1024; x++) for (int y = 0; y < 1024; y++) total += ocean[x][y] > 1;
+    printf("%d\n", total);
+}
---
+++ 05/README.md
@@ -0,0 +1,5 @@
+# Day 05: C
+
+Neither time nor coding inspiration today. Let's stick to good old C.
+
+PS. I know, the xor swapping trick is technically UB. Good enough for the puzzle though.
