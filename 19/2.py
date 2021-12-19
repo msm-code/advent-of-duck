e2cf75c574d18b1ae5054d968d1866c00c9d7805
msm <msm@tailcall.net>
2021-12-19 23:16:08 +0100

    Day 19: Python

---
+++ 19/2.py
@@ -0,0 +1,54 @@
+import itertools
+
+
+def rotate_xy(v): return (v[1], -v[0], v[2])
+def rotate_xz(v): return (v[2], v[1], -v[0])
+def rotate_yz(v): return (v[0], v[2], -v[1])
+def vsub(a, b): return (a[0] - b[0], a[1] - b[1], a[2] - b[2])
+def vadd(a, b): return (a[0] + b[0], a[1] + b[1], a[2] + b[2])
+
+
+def rotations(vec):
+    unique = set(next(iter(vec)))  # next(iter(set)) == first element
+    for xy in range(2):  # 2 are enough apparently
+        vec = [rotate_xy(v) for v in vec]
+        for xz in range(4):
+            vec = [rotate_xz(v) for v in vec]
+            for yz in range(4):
+                vec = [rotate_yz(v) for v in vec]
+                if next(iter(vec)) not in unique:
+                    unique.add(next(iter(vec)))  # (incorrect) check to ignore repeats
+                    yield vec
+
+
+def match(a, template):
+    for b in rotations(template):
+        for pa in a:
+            for pb in b:
+                shift = vsub(pa, pb)
+                if sum(vadd(p, shift) in a for p in b) >= 12:
+                    return a | set(vadd(p, shift) for p in b if vadd(p, shift) not in a), shift
+    return None
+
+
+scanners = open('input').read().split('scanner')
+scanners = [
+    set(tuple(int(c) for c in line.split(',')) for line in scanner.split("\n")[1:-1] if line)
+    for scanner in scanners if len(scanner.split("\n")) > 1
+]
+
+result = scanners.pop()
+shifts = [(0, 0, 0)]
+while scanners:
+    for scanner in scanners:
+        if glued := match(result, scanner):
+            result, shift = glued
+            scanners.remove(scanner)
+            shifts.append(shift)
+            break
+
+maxdist = 0
+for px in shifts:
+    for py in shifts:
+        maxdist = max(maxdist, sum(abs(c) for c in vsub(px, py)))
+print(maxdist)
---
+++ 19/README.md
@@ -0,0 +1,4 @@
+# Day 19: Python
+
+Not a very smart solution... Again.
+May not be the fastest, but hey, it gets the job done.
