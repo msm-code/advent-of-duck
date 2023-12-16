55a11fbed0a0eac2c4708e0ca78f06e086de8eaf
msm <msm@tailcall.net>
2023-12-16 18:14:46 +0100

    Day 16: Python

---
+++ 2023/16/1.py
@@ -0,0 +1,26 @@
+world = open("input", "r").read().strip().split("\n")
+
+dirmap = { "E": (1, 0), "W": (-1, 0), "S": (0, 1), "N": (0, -1) }
+def addv(a, b): return (a[0] + b[0], a[1] + b[1])
+def inworld(pos): return 0 <= pos[0] < len(world[0]) and 0 <= pos[1] < len(world)
+def follow(dir, pos): return addv(dirmap[dir], pos)
+
+def search(world):
+    queue, visited = [("E", (0, 0))], set([("E", (0, 0))])
+    while queue:
+        dir, pos = queue.pop()
+        elm = world[pos[1]][pos[0]]
+        if elm == '.': newdirs = [dir]
+        elif elm == '/': newdirs = { "E": ["N"], "N": ["E"], "S": ["W"], "W": ["S"] }[dir]
+        elif elm == '\\': newdirs = { "E": ["S"], "N": ["W"], "S": ["E"], "W": ["N"] }[dir]
+        elif elm == '-': newdirs = { "E": ["E"], "N": ["E", "W"], "S": ["E", "W"], "W": ["W"] }[dir]
+        elif elm == '|': newdirs = { "E": ["S", "N"], "N": ["N"], "S": ["S"], "W": ["S", "N"] }[dir]
+        else: assert False
+        for newdir in newdirs:
+            newpos = follow(newdir, pos)
+            if (newdir, newpos) in visited or not inworld(newpos): continue
+            visited.add((newdir, newpos))
+            queue.append((newdir, newpos))
+    return visited
+
+print(len(set(y for (x, y) in search(world))))
